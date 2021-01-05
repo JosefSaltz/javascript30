eb4d5068cb8796e4229c77945c144cd285402a31
Josef Saltz <jps.webdev@gmail.com>
2021-01-04 16:51:53 -0800

    Initial Commit

---
+++ d1-drumkit/script.js
@@ -0,0 +1,12 @@
+//TODO:
+//Random Drum Trigger Headers!
+//Drum Kit Swapper
+//gradient animation
+
+const titleAdLibs = ["Boom Boom Bap!", "Pew Pew Pew!", "All About Dat Bass!", "Hot Damn!", "Fire Beats Over Here!", "Check Out My Soundcloud!", "It's a Function Stack Yo!", "Skrrt Skrrt!", "Rip This Off Will.I.Am!"];
+
+window.addEventListener('keydown', function(e) {
+  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
+  e.
+});
