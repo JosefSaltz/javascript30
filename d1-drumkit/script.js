fbf728d427042ccae3c9641769232127ec8984e6
Josef Saltz <jps.webdev@gmail.com>
2021-01-04 20:10:20 -0800

    Added comments for Wes' Boilerplate

--- d1-drumkit/script.js
+++ d1-drumkit/script.js
@@ -3,7 +3,7 @@
 //Drum Kit Swapper
 //gradient animation
 
-const titleAdLibs = ["Boom Boom Bap!", "Pew Pew Pew!", "All About Dat Bass!", "Hot Damn!", "Fire Beats Over Here!", "Check Out My Soundcloud!", "It's a Function Stack Yo!", "Skrrt Skrrt!", "Rip This Off Will.I.Am!"];
+const titleAdLibs = ["Boom Boom Bap!", "Pew Pew Pew!", "All About Dat Bass!", "Hot Damn!", "Fire Beats Over Here!", "Check Out My Soundcloud!", "It's a Function Stack Yo!", "Skrrt Skrrt!", "Rip This Off Will.I.Am!", "Damn You!"];
 
 window.addEventListener('keydown', function(e) {
   console.log(e);
