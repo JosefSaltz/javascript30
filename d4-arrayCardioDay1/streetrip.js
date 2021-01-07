ca7bd632a0120b717e32d4b00b3e457719c87274
Josef Saltz <jps.webdev@gmail.com>
2021-01-06 18:38:39 -0800

    Whew!

---
+++ d4-arrayCardioDay1/streetrip.js
@@ -0,0 +1,75 @@
+const streetrip = {0: "Boulevards of Paris",
+​
+1: "City walls of Paris",
+​
+2: "Thiers wall",
+​
+3: "Wall of Charles V",
+​
+4: "Wall of Philip II Augustus",
+​
+5: "City gates of Paris"
+​
+6: "Haussmann's renovation of Paris"
+​
+7: "Boulevards of the Marshals"
+​
+8: "Boulevard Auguste-Blanqui"
+​
+9: "Boulevard Barbès"
+​
+10: "Boulevard Beaumarchais"
+​
+11: "Boulevard de l'Amiral-Bruix"
+​
+12: "Boulevard Mortier"
+​
+13: "Boulevard Poniatowski"
+​
+14: "Boulevard Soult"
+​
+15: "Boulevard des Capucines"
+​
+16: "Boulevard de la Chapelle"
+​
+17: "Boulevard de Clichy"
+​
+18: "Boulevard du Crime"
+​
+19: "Boulevard du Général-d'Armée-Jean-Simon"
+​
+20: "Boulevard Haussmann"
+​
+21: "Boulevard de l'Hôpital"
+​
+22: "Boulevard des Italiens"
+​
+23: "Boulevard Lefebvre"
+​
+24: "Boulevard de la Madeleine"
+​
+25: "Boulevard de Magenta"
+​
+26: "Boulevard Marguerite-de-Rochechouart"
+​
+27: "Boulevard Montmartre"
+​
+28: "Boulevard du Montparnasse"
+​
+29: "Boulevard Raspail"
+​
+30: "Boulevard Richard-Lenoir"
+​
+31: "Boulevard Saint-Germain"
+​
+32: "Boulevard Saint-Michel"
+​
+33: "Boulevard de Sébastopol"
+​
+34: "Boulevard de Strasbourg"
+​
+35: "Boulevard du Temple"
+​
+36: "Boulevard Voltaire"
+​
+37: "Boulevard de la Zone"}
