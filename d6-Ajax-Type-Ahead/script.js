a1ea6205483c6873d28e868287fcff66ee39462d
Josef Saltz <jps.webdev@gmail.com>
2021-01-10 01:03:39 -0800

    Excercise Complete

--- d6-Ajax-Type-Ahead/script.js
+++ d6-Ajax-Type-Ahead/script.js
@@ -1,1 +1,51 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+  //Blob must be converted from serialized data
+  .then(blob => { 
+    return blob.json();
+    
+  })
+  .then(data => {
+    cities.push(...data);
+    console.log(data);
+  });
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`);
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`);
+    return `
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+      <span class="name">${numberWithCommas(place.population)}</span>
+    </li>
+    `
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+//searchInput.addEventListener('')
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
